5282091945812fb06d41ebffe5e55a89182e13cc
Alex <amcisaac@me.com>
2024-06-16 19:23:30 -0400

    far cleaner functions for tab rendering. exit rendering now works

--- src/ui.rs
+++ src/ui.rs
@@ -25,43 +25,24 @@ pub fn render_ui(f: &mut Frame, app: &mut App) {
 
     //TITLE
     render_tab_frame(f, app, chunks[1]);
-    
-    //Dynamic body layout defenition (mutable) (now just splits the middle main chunk entirely
-    let field_layout = screen_layout(f, app, chunks[2]);
 
-    //REMOVED FEATURE, NOW IN TABS, todo? implement as something else later? IN PROGRESS
-    //render_menu(app, f, body_layout[0]);
+    //Dynamic body layout defenition (mutable) (now just splits the middle main chunk entirely
+    let field_layout = render_tab(f, app, chunks[2]);
 
     //FOOTER
     render_footer(f, app, chunks[3]);
     //Here, the middle segment of the screen is organized differently depending on the selected tab, text, and widget content is specified later
-    /*
-               if let Some(editing) = &app.currently_editing {
-                   match editing {
-                       CurrentlyEditing::Time => {
-                           Span::styled("Editing : Task Schedule", Style::default().fg(Color::Green))
-                       }
-                       CurrentlyEditing::Script => {
-                           Span::styled("Editing : PATH", Style::default().fg(Color::Blue))
-                       }
-                   }
-               } else {
-                   Span::styled("Editing : ", Style::default().fg(Color::DarkGray))
-               }
-           },
-       ];
 
-    */
-
-    //EXIT MENU
 }
 
-//Helper Functions
+
+
+//Helper Functions //
 //Menu Styling won't work??? Why???
 
-///new_tab_frame In development function to make outer box for the entire tab frame, use render title for now, called tab render 
+///new_tab_frame In development function to make outer box for the entire tab frame, use render title for now, called tab render
 fn new_tab_frame(f: &mut Frame, app: &mut App, area: Rect) {
-    
+
         let menu_style = Style::default().add_modifier(Modifier::BOLD).fg(Color::Yellow).bg(Color::Rgb(0,0,55));
         let mut menu_text = ratatui::prelude::Line::styled("DEFAULT", menu_style);
 
@@ -87,7 +68,7 @@ fn render_menu(app: &mut App, f: &mut Frame, area: Rect) {
         .highlight_symbol("»")
         .repeat_highlight_symbol(true);
     f.render_stateful_widget(main_menu, area, &mut app.tab_state);
-} 
+}
 fn render_toolbar(f: &mut Frame, app: &mut App, area: Rect) {
     let tabs = app.tabs.clone();
     let tab_bar = Tabs::new(tabs)
@@ -117,7 +98,7 @@ fn render_tab_frame(f: &mut Frame, app: &mut App, area: Rect) {
 
     let menu_style = Style::default().add_modifier(Modifier::BOLD).fg(Color::Yellow).bg(Color::Rgb(0,0,55));
     let mut menu_text = Text::raw("DEFAULT");
-    
+
 
     match app.selected_tab {
         CurrentTab::Menu => menu_text = Text::raw("Menu"),
@@ -127,95 +108,116 @@ fn render_tab_frame(f: &mut Frame, app: &mut App, area: Rect) {
         CurrentTab::Exit => menu_text = Text::raw("Exit"),
         _ => { }
     };
-    
+
     let menu_block = Paragraph::new(menu_text)
         .style(menu_style)
         .alignment(Alignment::Center)
         .block(Block::bordered());
-    
+
     f.render_widget(menu_block, area);
 }
-
-fn screen_layout(f: &mut Frame, app: &mut App, split_target: Rect) {
+fn render_tab(f: &mut Frame, app: &mut App, tab: Rect) {
     match app.selected_tab {
-        CurrentTab::Menu => {
-            let mut mod_layout = Layout::default()
-                .direction(Direction::Horizontal)
-                .constraints([Constraint::Length(25), Constraint::Percentage(80)])
-                .split(split_target);
-
-            let context =
-                Paragraph::new("Main menu context").block(Block::bordered().title("Main Menu"));
-            let other =
-                Paragraph::new("we are locked in!").block(Block::bordered().title("context"));
-            f.render_widget(context, mod_layout[0]);
-            f.render_widget(other, mod_layout[1]);
-        }
-        CurrentTab::Edit => {
-            let mut mod_layout = Layout::default()
-                .direction(Direction::Vertical)
-                .constraints([Constraint::Length(25), Constraint::Percentage(80)])
-                .split(split_target);
-
-            let context = Paragraph::new("Edit the tab").block(Block::bordered().title("Edit tab"));
-            let other = Paragraph::new("we are locked in!").block(Block::bordered().title("Time"));
-            f.render_widget(context, mod_layout[0]);
-            
-        }
-        CurrentTab::New => {
-            let mut mod_layout = Layout::default()
-                .direction(Direction::Horizontal)
-                .constraints([Constraint::Length(25), Constraint::Percentage(80)])
-                .split(split_target);
-
-            let context =
-                Paragraph::new("Main menu context").block(Block::bordered().title("context"));
-            let other = Paragraph::new("we are locked in!").block(Block::bordered().title("Other"));
-            f.render_widget(context, mod_layout[0]);
-            f.render_widget(other, mod_layout[1]);
-        }
-        CurrentTab::Options => {
-            let mut mod_layout = Layout::default()
-                .direction(Direction::Horizontal)
-                .constraints([
-                    Constraint::Min(4),
-                    Constraint::Percentage(50),
-                    Constraint::Min(4),
-                ])
-                .split(split_target);
-
-            let items = ["Item 1", "Item 2", "Item 3"];
-            let options = List::new(items)
-                .block(Block::bordered().title("Options List"))
-                .style(Style::default().fg(Color::White))
-                .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
-                .highlight_symbol(">>")
-                .repeat_highlight_symbol(true)
-                .direction(ListDirection::TopToBottom);
-            let mut option_state = ListState::default();
-            option_state.select(Some(0));
-            let context = Paragraph::new("Here's a quick description of what this item does")
-                .block(Block::bordered().title("Context"));
-            f.render_stateful_widget(options, mod_layout[0], &mut option_state);
-            f.render_widget(context, mod_layout[1]);
-        }
-
-        CurrentTab::Exit => {
-            let mut mod_layout = Layout::default()
-                .direction(Direction::Horizontal)
-                .constraints([Constraint::Percentage(80), Constraint::Percentage(20)])
-                .split(split_target);
-
-            let context =
-                Paragraph::new("Main menu context").block(Block::bordered().title("context"));
-            let other = Paragraph::new("we are locked in!").block(Block::bordered().title("Other"));
-            f.render_widget(context, mod_layout[0]);
-            f.render_widget(other, mod_layout[1]);
-        }
+        CurrentTab::Menu => menu_tab(f, app, tab),
+        CurrentTab::Edit => edit_tab(f, app, tab),
+        CurrentTab::New => new_tab(f, app, tab),
+        CurrentTab::Options => options_tab(f, app, tab),
+        CurrentTab::Exit => exit_tab(f, app, tab), //why does this work now, wtf???
     }
 }
-//dont overlay, get clear screen
-fn exit_menu(f: &mut Frame, app: &mut App) {
+
+//Tab Layouts //
+fn menu_tab(f: &mut Frame, app: &mut App, tab: Rect) {
+    let mut window = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Length(25), Constraint::Percentage(80)])
+        .split(tab);
+
+    let context = Paragraph::new("Main menu context").block(Block::bordered().title("context"));
+    let other = Paragraph::new("we are locked in!").block(Block::bordered().title("Other"));
+    f.render_widget(context, window[0]);
+    f.render_widget(other, window[1]);
+}
+fn new_tab(f: &mut Frame, app: &mut App, tab: Rect) {
+    let mut window = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Length(25), Constraint::Percentage(80)])
+        .split(tab);
+
+    let context =
+        Paragraph::new("Main menu context").block(Block::bordered().title("context"));
+    let other = Paragraph::new("we are locked in!").block(Block::bordered().title("Other"));
+    f.render_widget(context, window[0]);
+    f.render_widget(other, window[1]);
+
+    let schedule_block = Paragraph::new("NEW CRON").block(Block::bordered().title("Set Schedule"));
+    f.render_widget(schedule_block, window[0]);
+
+    let path_block = Paragraph::new("NEW Script Path : /home/pi/scripts/do.sh")
+        .block(Block::bordered().title("Script Path"));
+    f.render_widget(path_block, window[1]);
+}
+fn edit_tab(f: &mut Frame, app: &mut App, tab: Rect) {
+    let mut window = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Length(25), Constraint::Percentage(80)])
+        .split(tab);
+
+    let context = Paragraph::new("Edit the tab").block(Block::bordered().title("Edit tab"));
+    let other = Paragraph::new("we are locked in!").block(Block::bordered().title("Time"));
+    f.render_widget(context, window[0]);
+
+    let schedule_block = Paragraph::new("EDIT CRON").block(Block::bordered().title("Modify Schedule"));
+    f.render_widget(schedule_block, window[0]);
+
+    let path_block = Paragraph::new("EDIT Script Path : /home/pi/scripts/do.sh")
+        .block(Block::bordered().title("Script Path"));
+    f.render_widget(path_block, window[1]);
+}
+fn options_tab(f: &mut Frame, app: &mut App, tab: Rect) {
+    let mut window = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Min(4),
+            Constraint::Percentage(50),
+            Constraint::Min(4),
+        ])
+        .split(tab);
+
+    let items = ["Item 1", "Item 2", "Item 3"];
+    let options = List::new(items)
+        .block(Block::bordered().title("Options List"))
+        .style(Style::default().fg(Color::White))
+        .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
+        .highlight_symbol(">>")
+        .repeat_highlight_symbol(true)
+        .direction(ListDirection::TopToBottom);
+    let mut option_state = ListState::default();
+    option_state.select(Some(0));
+    let context = Paragraph::new("Here's a quick description of what this item does")
+        .block(Block::bordered().title("Context"));
+    f.render_stateful_widget(options, window[0], &mut option_state);
+    f.render_widget(context, window[1]);
+
+    let schedule_block = Paragraph::new("EDIT CRON").block(Block::bordered().title("Modify Schedule"));
+    f.render_widget(schedule_block, window[0]);
+
+    let path_block = Paragraph::new("EDIT Script Path : /home/pi/scripts/do.sh")
+        .block(Block::bordered().title("Script Path"));
+    f.render_widget(path_block, window[1]);
+}
+fn exit_tab(f: &mut Frame, app: &mut App, tab: Rect) {
+    let mut window = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(80), Constraint::Percentage(20)])
+        .split(tab);
+
+    let context =
+        Paragraph::new("Main menu context").block(Block::bordered().title("context"));
+    let other = Paragraph::new("we are locked in!").block(Block::bordered().title("Other"));
+    f.render_widget(context, window[0]);
+    f.render_widget(other, window[1]);
+
     f.render_widget(Clear, f.size()); //this clears the entire screen and anything already drawn
     let popup_block = Block::default()
         .title("Y/N")
@@ -236,8 +238,20 @@ fn exit_menu(f: &mut Frame, app: &mut App) {
     f.render_widget(exit_paragraph, area);
 }
 
-//generates the popup menu, does not pass to renderer
-fn exit_tab(f: &mut Frame, app: &mut App) {
+//Broken or unused functions //
+fn exiter(f: &mut Frame, app: &mut App, window: Rect) {
+    let mut window = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(80), Constraint::Percentage(20)])
+        .split(window);
+
+    let context =
+        Paragraph::new("Main menu context").block(Block::bordered().title("context"));
+    let other = Paragraph::new("we are locked in!").block(Block::bordered().title("Other"));
+
+    f.render_widget(context, window[0]);
+    f.render_widget(other, window[1]);
+
     f.render_widget(Clear, f.size()); //this clears the entire screen and anything already drawn
     let popup_block = Block::default()
         .title("Y/N")
@@ -256,31 +270,6 @@ fn exit_tab(f: &mut Frame, app: &mut App) {
 
     let area = centered_rect(60, 25, f.size());
 }
-
-fn new_tab(f: &mut Frame, app: &mut App, layout: Rc<[Rect]>) {
-    let schedule_block = Paragraph::new("NEW CRON").block(Block::bordered().title("Set Schedule"));
-    f.render_widget(schedule_block, layout[0]);
-
-    let path_block = Paragraph::new("NEW Script Path : /home/pi/scripts/do.sh")
-        .block(Block::bordered().title("Script Path"));
-    f.render_widget(path_block, layout[1]);
-}
-
-fn edit_tab(f: &mut Frame, app: &mut App, layout: Rc<[Rect]>) {
-    let schedule_block = Paragraph::new("EDIT CRON").block(Block::bordered().title("Modify Schedule"));
-    f.render_widget(schedule_block, layout[0]);
-
-    let path_block = Paragraph::new("EDIT Script Path : /home/pi/scripts/do.sh")
-        .block(Block::bordered().title("Script Path"));
-    f.render_widget(path_block, layout[1]);
-}
-fn menu_tab(f: &mut Frame, app: &mut App, layout: Rc<[Rect]>) {
-    let context = Paragraph::new("Main menu context").block(Block::bordered().title("context"));
-    let other = Paragraph::new("we are locked in!").block(Block::bordered().title("Other"));
-    f.render_widget(context, layout[0]);
-    f.render_widget(other, layout[1]);
-}
-
 /// helper function to create a centered rect using up certain percentage of the available rect `r`
 fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
     // Cut the given rectangle into three vertical pieces
